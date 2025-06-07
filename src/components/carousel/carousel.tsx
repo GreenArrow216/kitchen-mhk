4ed7dc75151846a5e9bfffc71157d27fea4857e7
Green Arrow <gowthamanganesan021@gmail.com>
2025-06-07 13:54:40 +0100

    carousel responsiveness completed

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,5 +1,20 @@
-const Carousel = () => {
-    return <h1>Carousel</h1>
-}
+import "./carousel.scss"
 
-export default Carousel
+const Carousel = () => {
+  return (
+    <div className="carousel">
+      <div className="content">
+        <p>DESIGN AND ORDER YOUR NEW KITCHEN ONLINE TODAY</p>
+        <h1>Bespoke & made to measure handmade kitchen design</h1>
+        <button className="lg">ORDER NOW</button>
+      </div>
+      <div className="page-indicator">
+        <span className="active"/>
+        <span/>
+        <span/>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
