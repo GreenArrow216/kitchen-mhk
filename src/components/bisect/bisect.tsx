1377dfde500f3e48b384a58c3de6a340a06de54e
Green Arrow <gowthamanganesan021@gmail.com>
2025-06-07 15:41:03 +0100

    bisect section responsiveness completed

--- src/components/bisect/bisect.tsx
+++ src/components/bisect/bisect.tsx
@@ -1,5 +1,24 @@
-const Bisect = () => {
-    return <h1>Bisect</h1>
-}
+import './bisect.scss'
 
-export default Bisect
+const Bisect = () => {
+  return (
+    <div className="bisect">
+      <div className="img-container"/>
+      <div className="bisect-content">
+        <div className="discover">
+          <p>QUALITY CRAFTMANSHIP FROM BUILD TO DELIVERY</p>
+          <h2>Discover the beauty of a handmade kitchen</h2>
+          <p className="desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget
+            est sit amet sapien venenatis maximus vel in urna. Nam mauris arcu,
+            feugiat in finibus vitae, sollicitudin id purus. Ut imperdiet, magna
+            eu pharetra tincidunt, mauris purus ultrices.
+          </p>
+          <button className={'lg'}>ABOUT US</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bisect;
